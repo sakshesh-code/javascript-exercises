47ec36b35add64b6297e1b702e1ddd0b5d1212e4
Sakshesh Tuladhar <285657341+sakshesh-code@users.noreply.github.com>
2026-06-09 11:51:26 +0545

    update: Complete TOP JavaScript Foundations exercises 10 and 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //x °F ≘ (x − 32) × ⁠5/9⁠ °C 
+
+  const celsius = ((fahrenheit - 32) * (5/9));
+  return Number.isInteger(celsius) ? 
+    celsius :Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //x °C ≘ (x × ⁠9/5⁠ + 32) °F 
+  const fahrenheit = (celsius * 9/5 + 32);
+  return Number.isInteger(fahrenheit) ? 
+    fahrenheit : Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
@@ -9,3 +18,4 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
