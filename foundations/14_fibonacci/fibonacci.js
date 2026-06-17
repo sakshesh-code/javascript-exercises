173ac7ec10f771f658459e43a0634cae97c734b7
Sakshesh Tuladhar <285657341+sakshesh-code@users.noreply.github.com>
2026-06-17 12:03:22 +0545

    Complete TOP JavaScript Foundations exercise 14 - 16

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
 
+    nth = Number(nth);
+
+    if(nth < 0){
+        return "OOPS";
+    }
+
+    let a = 0;
+    let b = 1;
+
+    for(let i = 1; i <= nth; i++){
+    
+        [a, b] = [b, a + b]; 
+
+    };
+
+    return a;
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+   return people.reduce((oldest, person)=>{
+            const currentDate = new Date().getFullYear();
 
+            let currentBirth = person.yearOfBirth;
+            let currentDeath = person.yearOfDeath || currentDate;
+
+            let oldestBirth = oldest.yearOfBirth;
+            let oldestDeath = oldest.yearOfDeath || currentDate;
+
+            if(currentDeath - currentBirth > oldestDeath - oldestBirth){
+                oldest = person;
+            }
+
+        return oldest;
+    })
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
