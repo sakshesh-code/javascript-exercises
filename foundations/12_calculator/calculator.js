26670265b6c27de53a79f5269a54fe99821830e6
Sakshesh Tuladhar <285657341+sakshesh-code@users.noreply.github.com>
2026-06-17 08:11:43 +0545

    Complete TOP JavaScript foundations exercise 12 and 13

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, num)=> sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((multiple, num)=> multiple * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+
+	for(let i = num; i > 0; i--){
+    factorial *= i ; 
+  };
+
+  return factorial;
 };
+
 
 // Do not edit below this line
 module.exports = {
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const word = string.toLowerCase().split("");
+    const exclude = ",<.>/?\"':;|)}`~!@#$%^&*()_-+={[]\\ "
+    const filtered = word.filter(w=> !exclude.includes(w));
+
+    const reversed = [...filtered].reverse();
+
+    return filtered.join("") === reversed.join("") ? true : false;
 };
 
 // Do not edit below this line
